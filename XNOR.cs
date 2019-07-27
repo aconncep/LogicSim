b0a60f7d3262f62cfe0937d4e1501de2f58689af
Austin Cepalia <austincepalia@gmail.com>
2019-07-26 23:28:50 -0400

    Add support for XOR and XNOR

---
+++ XNOR.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace LogicSim.Commands
+{
+    public class XNOR : ICommand
+    {
+        Variable[] variables;
+
+        public XNOR() { }
+
+        public int ExpectedNumArguments()
+        {
+            return 2;
+        }
+
+        public void SetVariableValues(Variable[] vars)
+        {
+            variables = new Variable[] { vars[0], vars[1] };
+        }
+
+        public int Evaluate()
+        {
+            if (variables[0].value == 0 && variables[1].value == 1 || variables[0].value == 1 && variables[1].value == 0)
+            {
+                return 0;
+            }
+            return 1;
+        }
+
+        public Variable[] GetVariables()
+        {
+            return variables;
+        }
+
+        public override string ToString()
+        {
+            return "XNOR";
+        }
+    }
+}
---
+++ XOR.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace LogicSim.Commands
+{
+    public class XOR : ICommand
+    {
+        Variable[] variables;
+
+        public XOR() { }
+
+        public int ExpectedNumArguments()
+        {
+            return 2;
+        }
+
+        public void SetVariableValues(Variable[] vars)
+        {
+            variables = new Variable[] { vars[0], vars[1] };
+        }
+
+        public int Evaluate()
+        {
+            if (variables[0].value == 0 && variables[1].value == 1 || variables[0].value == 1 && variables[1].value == 0)
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+        public Variable[] GetVariables()
+        {
+            return variables;
+        }
+
+        public override string ToString()
+        {
+            return "XOR";
+        }
+    }
+}
